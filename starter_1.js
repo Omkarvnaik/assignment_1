fd8a3a6dd1c473a1e19bfa8895172ed9782acabc
Omkarvnaik <139659927+Omkarvnaik@users.noreply.github.com>
2023-07-18 18:25:16 -0700

    Add files via upload

---
+++ starter_1.js
@@ -0,0 +1,33 @@
+export class Starter {
+    /**
+     * The following exercises will help familiarize you with coding using
+     * primitive types (booleans, numbers, strings, etc.) in JavaScript. These
+     * exercises will also help familiarize you with JavaScript keywords,
+     * statements, and operators, as described throughout Assignment 1.
+     */
+
+    AddTwoNumbers(a, b) {
+        // TODO: Implement
+        return 0        
+    }
+
+    SubtractBFromA(a, b) {
+        // TODO: Implement
+        return 0
+    }
+
+    MultiplyTwoNumbers(a, b) {
+        // TODO: Implement
+        return 0
+    }
+
+    DivideAByB(a, b) {
+        // TODO: Implement
+        return 0
+    }
+
+    CalculateAverageOfTwoNumbers(a, b) {
+        // TODO: Implement
+        return 0
+    }
+}
