5e097511a5623f19f009420de8bf4759344d64c9
Omkarvnaik <139659927+Omkarvnaik@users.noreply.github.com>
2023-07-21 01:31:21 +0000

    Assignment 1 Finished

--- starter_1.js
+++ starter_1.js
@@ -1,4 +1,4 @@
-export class Starter {
+class Starter {
     /**
      * The following exercises will help familiarize you with coding using
      * primitive types (booleans, numbers, strings, etc.) in JavaScript. These
@@ -8,26 +8,52 @@ export class Starter {
 
     AddTwoNumbers(a, b) {
         // TODO: Implement
-        return 0        
+        return a+b        
     }
 
     SubtractBFromA(a, b) {
         // TODO: Implement
-        return 0
+        return b-a
     }
 
     MultiplyTwoNumbers(a, b) {
         // TODO: Implement
-        return 0
+        return a*b
     }
 
     DivideAByB(a, b) {
         // TODO: Implement
-        return 0
+        return a/b
     }
 
     CalculateAverageOfTwoNumbers(a, b) {
         // TODO: Implement
-        return 0
+        return (a+b)/2
     }
 }
+
+let starter=new Starter ()
+
+let addvalue=starter.AddTwoNumbers (1,2)
+console.log(addvalue)
+
+let subtractvalue = starter.SubtractBFromA (1,2)
+console.log(subtractvalue)
+
+let multiplyvalue = starter.MultiplyTwoNumbers (1,2)
+console.log(multiplyvalue)
+
+let dividevalue = starter.DivideAByB (1,2)
+console.log(dividevalue)
+
+dividevalue = starter.DivideAByB (2,2)
+console.log(dividevalue)
+
+dividevalue = starter.DivideAByB (3,2)
+console.log(dividevalue)
+
+dividevalue = starter.DivideAByB (-4,0)
+console.log(dividevalue)
+
+let averagevalue = starter.CalculateAverageOfTwoNumbers (1,2)
+console.log(averagevalue)
